f232796146599ce7d970e36a49497c87d585e0f0
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-07 10:34:13 +0300

    Tag model skeleton.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    /**
+     * Fields that allowed for autofill (not required).
+     *
+     * @var string[]
+     */
+    protected $fillable = ['label'];
+
+    /**
+     * Fields that not allowed for autofill (not required).
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Returns tag articles (OneToMany relation).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function articles()
+    {
+        $this->belongsToMany(Article::class);
+    }
 }
