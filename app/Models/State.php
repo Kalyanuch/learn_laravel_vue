40512c5f68a17f873c0dd7e14f182442eae607c0
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-07 10:32:42 +0300

    State model skeleton.

--- app/Models/State.php
+++ app/Models/State.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     use HasFactory;
+
+    /**
+     * Fields that allowed for autofill (not required).
+     *
+     * @var string[]
+     */
+    protected $fillable = ['likes', 'views', 'article_id'];
+
+    /**
+     * Fields that not allowed for autofill (not required).
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
