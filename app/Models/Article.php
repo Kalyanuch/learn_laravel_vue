0206830c46ca74c88df8baab2e63a66b3c1ffca2
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-07 10:29:42 +0300

    Article model skeleton.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,48 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    /**
+     * Fields that allowed for autofill (not required).
+     *
+     * @var string[]
+     */
+    protected $fillable = ['title', 'body', 'img', 'slug'];
+
+    /**
+     * Fields that not allowed for autofill (not required).
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Returns article comments (OneToMany relation).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Returns article statistics (OneToOne relation).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function state()
+    {
+        return $this->hasOne(State::class);
+    }
+
+    /**
+     * Returns article tags (ManyToMany relation).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
