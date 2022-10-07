982e4adf3fa5e5ed87a6e1e6c415975ca01015c6
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-07 11:56:21 +0300

    Fix homepage template and access to files.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,12 +24,12 @@
                 <div class="container-fluid">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link menu-link {{ $mainLink }} "
-                               href=" {{ route('home')  }}">Главная страница</a>
+                            <a class="nav-link menu-link "
+                               href="#">Главная страница</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link {{ $articleLink }}"
-                               href="{{ route('article.index')  }}">Каталог статей</a>
+                            <a class="nav-link menu-link"
+                               href="#">Каталог статей</a>
                         </li>
                     </ul>
                     <a class="d-flex justify-content-end " href="https://github.com/rageserg">
@@ -37,7 +37,7 @@
                     </a>
                 </div>
             </nav>
-            <div class="hero" style="background-image: url(/img/lorenzo-herrera.jpg);">
+            <div class="hero" style="background-image: url('img/lorenzo-herrera.jpg');">
                 <div class="container h-100">
                     <div class="row h-100 align-items-center">
                         <div class="col-12">
