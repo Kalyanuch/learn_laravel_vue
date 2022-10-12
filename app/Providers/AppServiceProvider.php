716d3f5845572391b3e0075542557e6042d3dcb2
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-12 15:36:39 +0300

    Move active menu logic from helper to provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
+    const HEADER_MENU_ACTIVE_CLASS = 'menu-link__active';
+
     /**
      * Register any application services.
      *
@@ -25,5 +29,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        $this->activeLinks();
+    }
+
+    public function activeLinks()
+    {
+        View::composer('layouts.app', function($view) {
+            $view->with('mainLink', request()->is('/') ? self::HEADER_MENU_ACTIVE_CLASS : '');
+            $view->with('articleLink', Route::current()->getName() == 'article.index' ? self::HEADER_MENU_ACTIVE_CLASS : '');
+        });
     }
 }
