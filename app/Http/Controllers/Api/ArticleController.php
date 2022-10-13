02de6654c4090985463de090eeabe43a7ee42ef0
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-13 18:02:11 +0300

    Test API article controller.

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function show()
     {
-        return 123;
+        $article = Article::with('comments', 'tags', 'state')->first();
+
+        return new ArticleResource($article);
     }
 }
