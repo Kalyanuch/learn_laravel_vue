5630fa95014305b20ea100d67b98a0df9e9661ef
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-19 14:28:23 +0300

    Use request for new comment validation.

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,25 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Comment\CreateRequest;
 
+/**
+ * Implements comments functionality.
+ */
 class CommentController extends Controller
 {
-    public function store(Request $request)
-    {
-        $validator = \Validator::make($request->all(), [
-            'subject' => 'required|min:6',
-            'body' => 'required|min:10',
-        ]);
 
-        if($validator->fails())
-        {
-            return response()->json([
-                'status' => 'error',
-                'msg' => 'Error',
-                'errors' => $validator->errors()
-            ], 422);
-        }
+    /**
+     * Adding new comment.
+     *
+     * @param CreateRequest $request
+     *   Create comment request.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *   Json response.
+     */
+    public function store(CreateRequest $request)
+    {
+
 
 
 
