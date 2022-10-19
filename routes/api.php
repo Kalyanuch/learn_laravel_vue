b1d478e3f62ca350544ed9ddad40fd7b0216c53c
Shamrylo Mykola <itlutsk@gmail.com>
2022-10-19 15:31:27 +0300

    Show 404 error page when user try to use api routes (post and put) in browser.

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,7 @@ Route::put('article-views-increment', [\App\Http\Controllers\Api\ArticleControll
 Route::put('article-likes-increment', [\App\Http\Controllers\Api\ArticleController::class, 'likesIncrement']);
 
 Route::post('article-add-comment', [\App\Http\Controllers\Api\CommentController::class, 'store']);
+
+Route::fallback(function() {
+    abort(404);
+});
